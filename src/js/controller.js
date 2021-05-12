c00b5242d9445433b59559e206ecda4b9aff0275
kusaldushmantha <kusald.14@cse.mrt.ac.lk>
2021-05-12 20:28:24 +0530

    Updating DOM elements without re-rendering implemented

--- src/js/controller.js
+++ src/js/controller.js
@@ -17,6 +17,8 @@ const controlRecipes = async function () {
 
     recipeView.renderSpinner();
 
+    resultsView.update(model.getSearchResultsPage());
+
     await model.loadRecipe(id);
 
     recipeView.render(model.state.recipe);
@@ -51,7 +53,7 @@ const controlPagination = function (gotoPageNumber) {
 
 const controlServings = function (newServings) {
   model.updateServings(newServings);
-  recipeView.render(model.state.recipe);
+  recipeView.update(model.state.recipe);
 };
 
 const init = function () {
